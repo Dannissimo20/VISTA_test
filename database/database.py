325c564b5923543f3f50189ef0a4a8219c50da68
Dannissimo20 <Denicha_d_89@mail.ru>
2025-06-24 11:17:55 +0300

    Rewrite database.py to class pattern

--- database/database.py
+++ database/database.py
@@ -1,11 +1,37 @@
+from contextlib import AbstractContextManager, contextmanager
+from typing import ContextManager
 import os
 from sqlalchemy import Engine, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DB_URL = os.environ.get("PG_DSN")
-engine: Engine = create_engine(DB_URL)
+# engine: Engine = create_engine(DB_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
+
+
+class DBWriter:
+    def __init__(self, dsn: str | None = None):
+        self.engine = create_engine(dsn or DB_URL)
+
+    def session(self) -> AbstractContextManager:
+        return get_session(sessionmaker(self.engine))
+
+
+@contextmanager  # type: ignore
+def get_session(maker: sessionmaker[Session]) -> ContextManager[Session]:  # type: ignore
+    with maker.begin() as session:
+        try:
+            yield session  # type: ignore
+        except Exception as e:
+            print(f"Session {maker} rollback: {e}")
+            session.rollback()
+            raise
