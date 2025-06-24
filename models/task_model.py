109d43aacc4c13ba0acd387e5a18753dc2338c1f
Dannissimo20 <Denicha_d_89@mail.ru>
2025-06-24 11:34:38 +0300

    Rewrite field id, add autowrite time for created at and updatedat fields

--- models/task_model.py
+++ models/task_model.py
@@ -1,12 +1,21 @@
-from sqlalchemy import UUID, Boolean, Column, DateTime, String
+import binascii
+import os
+import uuid
+from sqlalchemy import UUID, Boolean, Column, DateTime, String, func
 from database.database import Base
 
 
 class TaskModel(Base):
     __tablename__ = "task"
-    id = Column(UUID, primary_key = True)
+
+    id = Column(
+        String(16),
+        primary_key=True,
+        default=lambda: binascii.hexlify(os.urandom(8)).decode()
+    )
+    
     title = Column(String)
     text = Column(String)
-    checked = Column(Boolean)
-    createdat = Column(DateTime)
-    updatedat = Column(DateTime)
+    checked = Column(Boolean, nullable=False, default=False)
+    createdat = Column(DateTime, server_default=func.now())
+    updatedat = Column(DateTime, server_default=func.now(), onupdate=func.now())
